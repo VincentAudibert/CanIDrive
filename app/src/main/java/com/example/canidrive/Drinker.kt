21afad88352a8baec1eb90b47bb6cc908c628e1e
VincentAudibert <vincent.audib@gmail.com>
2019-11-19 11:08:07 +0100

    Documented rules for a drinker alcohol rate calculation.

--- app/src/main/java/com/example/canidrive/Drinker.kt
+++ app/src/main/java/com/example/canidrive/Drinker.kt
@@ -3,6 +3,20 @@ package com.example.canidrive
 import java.util.*
 import kotlin.collections.ArrayList
 
+
+const val ALCOHOL_DENSITY = 0.8
+
+/**
+ * Represents the person drinking.
+ * The parameters such as weight and sex may change as the user adjusts the inputs.
+ *
+ * Full rules :
+ *  - it takes 30min to reach max alcohol rate (1h if eating before)
+ *  - rate is : drink_volume * alcohol_degree * alcohol_density / (sex_factor * weight)
+ *  - sex_factor is 0.6 for women, 0.7 for men
+ *  - decrease is 0.085-0.1g/L/h for women, 0.1-0.15g/L/h for men.
+ */
+
 class Drinker(var weight: Double = 80.0, var sex: String = "NONE") {
 
     private val absorbedDrinks : MutableList<Drink> = ArrayList()
@@ -15,7 +29,7 @@ class Drinker(var weight: Double = 80.0, var sex: String = "NONE") {
 
     fun alcoholLevel(date: Date): Double {
         var alcoholMass = absorbedDrinks.map {
-                drink -> drink.degree * drink.qtyMilliLiter * 0.8 / 100
+                drink -> drink.degree / 100 * drink.qtyMilliLiter * ALCOHOL_DENSITY
         } .sum()
 
         return alcoholMass / (sexFactor() * weight)
