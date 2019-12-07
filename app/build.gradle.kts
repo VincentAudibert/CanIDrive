2662e556b0b0e7259265a430e60a53b24b9a1014
VincentAudibert <vincent.audib@gmail.com>
2019-12-07 20:26:45 +0100

    Reworked gradle scripts to execute unit tests.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,9 +27,13 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation( "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
-    implementation( "androidx.appcompat:appcompat:1.1.0")
+
+    // Kotlin
+    implementation( kotlin("stdlib-jdk8", version = "1.3.61"))
     implementation( "androidx.core:core-ktx:1.1.0")
+
+    // Android
+    implementation( "androidx.appcompat:appcompat:1.1.0")
     implementation( "androidx.constraintlayout:constraintlayout:1.1.3")
     implementation( "com.google.android.material:material:1.0.0")
     implementation( "androidx.legacy:legacy-support-v4:1.0.0")
@@ -37,11 +41,20 @@ dependencies {
     // JUnit
     testImplementation( "org.junit.jupiter:junit-jupiter-api:5.5.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-
     androidTestImplementation("androidx.test:runner:1.2.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 
-    // Kotlin
+    // Kotlin needs for testing
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+
+    // Navigation (Kotlin)
     implementation( "androidx.navigation:navigation-fragment-ktx:2.1.0")
     implementation( "androidx.navigation:navigation-ui-ktx:2.1.0")
+}
+
+tasks.withType<Test> {
+    // Use the native JUnit support of Gradle.
+    useJUnitPlatform()
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
         jcenter()
+        mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.3")
-        classpath( "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-
+        classpath(kotlin("gradle-plugin", version = "1.3.61"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0")
-
     }
 }
 
@@ -20,7 +15,6 @@ allprojects {
     repositories {
         google()
         jcenter()
-        
     }
 }
 
