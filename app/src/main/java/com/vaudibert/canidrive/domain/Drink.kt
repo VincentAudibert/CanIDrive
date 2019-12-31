01f2bb029f69af0435fe73c0487c3e32697db66f
VincentAudibert <vincent.audib@gmail.com>
2019-12-31 15:07:02 +0100

    Lint corrections

--- app/src/main/java/com/vaudibert/canidrive/domain/Drink.kt
+++ app/src/main/java/com/vaudibert/canidrive/domain/Drink.kt
@@ -1,16 +1,10 @@
 package com.vaudibert.canidrive.domain
 
-import java.text.SimpleDateFormat
 import java.util.*
 
 const val ALCOHOL_DENSITY = 0.8
 
 data class Drink(val volume:Double, val degree:Double, val ingestionTime: Date) {
     fun alcoholMass(): Double = degree/100 * volume * ALCOHOL_DENSITY
-
-    override fun toString(): String {
-        val dateFormat = SimpleDateFormat("HH:mm")
-        return "You drank $volume mL at $degree% at " + dateFormat.format(ingestionTime)
-    }
 }
 
