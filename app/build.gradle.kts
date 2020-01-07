b655c11d0c2c3d4adbeb8a5f379e06a41c856d46
Vincent AUDIBERT <35406563+VincentAudibert@users.noreply.github.com>
2020-01-07 09:29:17 +0100

    #75 bac update on country change (#78)
    * Correct bug BAC update on country selection change
    
    * Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vaudibert.canidrive"
         minSdkVersion(19)
         targetSdkVersion(28)
-        versionCode = 7
-        versionName = "0.0.6"
+        versionCode = 8
+        versionName = "0.0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
