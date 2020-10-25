d1fdd3b59f07b955e29b656b7524b6ae58af2781
Vincent AUDIBERT <35406563+VincentAudibert@users.noreply.github.com>
2020-10-25 15:27:20 +0100

    Move to required API 30 (#110)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 }
 
 android {
-    compileSdkVersion(28)
+    compileSdkVersion(30)
     defaultConfig {
         applicationId = "com.vaudibert.canidrive"
         minSdkVersion(19)
-        targetSdkVersion(28)
-        versionCode = 17
-        versionName = "0.2.4"
+        targetSdkVersion(30)
+        versionCode = 18
+        versionName = "0.2.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         kapt {
             arguments {
@@ -42,19 +42,19 @@ dependencies {
 
     // Kotlin
     implementation(kotlin("stdlib-jdk8", version = "1.3.61"))
-    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("androidx.core:core-ktx:1.3.2")
 
     // Android
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+    implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
     // JUnit
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.test:runner:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 
     // Kotlin needs for testing
     implementation(kotlin("reflect"))
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("com.android.tools.build:gradle:4.0.1")
         classpath(kotlin("gradle-plugin", version = "1.3.61"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0")
     }
