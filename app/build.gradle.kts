81c21159f61d7016c0669f97de996af901be695b
Vincent@Mindflow <vincent@mindflow.so>
2021-11-07 15:41:48 +0100

    bump versions (merge error)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vaudibert.canidrive"
         minSdkVersion(19)
         targetSdkVersion(30)
-        versionCode = 18
-        versionName = "0.2.5"
+        versionCode = 19
+        versionName = "0.2.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         kapt {
             arguments {
