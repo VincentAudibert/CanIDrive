4f0b05924dc1212e9a8a682e50020c407e6f389e
Vincent-Mindflow <vincent@mindflow.so>
2022-05-13 18:34:25 +0200

    bump version 0.2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vaudibert.canidrive"
         minSdkVersion(19)
         targetSdkVersion(30)
-        versionCode = 20
-        versionName = "0.2.7"
+        versionCode = 22
+        versionName = "0.2.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         kapt {
             arguments {
